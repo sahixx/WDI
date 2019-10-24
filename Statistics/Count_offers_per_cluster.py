46f262a46ece2d3d2afc441d7a6ad701b1a18a35
hyan1993 <hyan@mail.uni-mannheim.de>
2019-10-25 01:16:01 +0200

    Delete Count_offers_per_cluster.py

--- Statistics/Count_offers_per_cluster.py
+++
@@ -1,9 +0,0 @@
-import csv
-import json
-import pandas as pd
-
-data = pd.read_csv('Final.csv')
-twocoldata = data[['cluster_id', 'url']]
-grouped = twocoldata.groupby('cluster_id', as_index=False)['url'].count()
-
-df = grouped.to_csv('offers_per_cluster.csv', index=False)
