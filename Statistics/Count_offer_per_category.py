8d28ab9fb8d58de9395d65af13b30baa35b45a69
hyan1993 <hyan@mail.uni-mannheim.de>
2019-10-25 01:15:50 +0200

    Delete Count_offer_per_category.py

--- Statistics/Count_offer_per_category.py
+++
@@ -1,9 +0,0 @@
-import csv
-import json
-import pandas as pd
-
-data = pd.read_csv('Final.csv')
-twocoldata = data[['cluster_id', 'offers']]
-grouped = twocoldata.groupby('cluster_id', as_index=False)['kvp_count'].sum()
-
-df = grouped.to_csv('offers_per_category.csv', index=False)
